fd08b779851f7ff6f544506f76fe5dacf663748e
Maxximusan <dragonstonecave@gmail.com>
2023-04-06 23:32:15 +0300

    2.2 заклнчил тест по материалам. Осталась непонятка с ошибкой(

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import {
   StyleSheet,
@@ -7,64 +7,149 @@ import {
   ImageBackground,
   TextInput,
   TouchableOpacity,
-  Platform,
+  // Platform,
   // KeyboardAvoidingView,
   Keyboard,
+  TouchableWithoutFeedback,
+  Dimensions,
 } from "react-native";
 
+// import * as Font from "expo-font";
+// import { AppLoading } from "expo";
+
+import { useFonts } from "expo-font";
+import { useCallback } from "react";
+import * as SplashScreen from "expo-splash-screen";
+
+const initialState = {
+  email: "",
+  password: "",
+};
+
+// const loadFonts = async () => {
+//   await Font.loadAsync({
+//     "roboto-400": require("./assets/fonts/Roboto-Regular.ttf"),
+//     "roboto-500": require("./assets/fonts/Roboto-Medium.ttf"),
+//     "roboto-700": require("./assets/fonts/Roboto-Bold.ttf"),
+//   });
+// };
+
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
-  const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+  // const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+  const [state, setstate] = useState(initialState);
+  // const [isReadyForFonts, setIsReadyForFonts] = useState(false);
+  const [dimensions, setdimensions] = useState(
+    Dimensions.get("window").width - 20 * 2
+  );
+
+  const [fontsLoaded] = useFonts({
+    "roboto-400": require("./assets/fonts/Roboto-Regular.ttf"),
+    "roboto-500": require("./assets/fonts/Roboto-Medium.ttf"),
+    "roboto-700": require("./assets/fonts/Roboto-Bold.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  useEffect(() => {
+    const onChange = () => {
+      const width = Dimensions.get("window").width - 20 * 2;
+
+      // console.log("width", width);
+      setdimensions(width);
+    };
+    Dimensions.addEventListener("change", onChange);
+    return () => {
+      Dimensions.removeEventListener("change", onChange);
+    };
+  }, []);
 
   const keyboardHide = () => {
-    setIsShowKeyboard(false);
+    // setIsShowKeyboard(false);
     Keyboard.dismiss();
+    console.log(state);
+    setstate(initialState);
   };
 
+  // if (!isReadyForFonts) {
+  //   return (
+  //     <AppLoading
+  //       startAsync={loadFonts}
+  //       onFinish={() => setIsReadyForFonts(true)}
+  //       onError={console.warn}
+  //     />
+  //   );
+  // }
+
   return (
-    <View style={styles.container}>
-      <ImageBackground
-        style={styles.image}
-        source={require("./assets/images/screen-2.webp")}
-      >
-        {/* <KeyboardAvoidingView
+    <TouchableWithoutFeedback onPress={keyboardHide}>
+      <View style={styles.container} onLayout={onLayoutRootView}>
+        <ImageBackground
+          style={styles.image}
+          source={require("./assets/images/screen-2.webp")}
+        >
+          {/* <KeyboardAvoidingView
           behavior={Platform.OS === "ios" ? "padding" : "height"}
         > */}
-        <View
-          style={styles.form}
-          // style={{ ...styles.form, marginBottom: isShowKeyboard ? 40 : 100 }}
-        >
-          <View style={{ marginBottom: 20 }}>
-            <Text style={styles.inputTitle}>EMAIL</Text>
-            <TextInput
-              style={styles.input}
-              textAlign={"center"}
-              // onFocus={() => setIsShowKeyboard(true)}
-            />
-          </View>
-          <View style={{ marginBottom: 20 }}>
-            <Text style={styles.inputTitle}>PASSWORD</Text>
-            <TextInput
-              style={styles.input}
-              textAlign={"center"}
-              secureTextEntry={true}
-              // onFocus={() => setIsShowKeyboard(true)}
-            />
-          </View>
-          <TouchableOpacity
-            style={styles.btn}
-            activeOpacity={0.4}
-            onPress={keyboardHide}
+          <View
+            style={{ ...styles.form, width: dimensions }}
+            // style={{ ...styles.form, marginBottom: isShowKeyboard ? 40 : 100 }}
           >
-            <Text style={styles.btnTitle}>SING IN</Text>
-          </TouchableOpacity>
-        </View>
-        {/* </KeyboardAvoidingView> */}
-        {/* <View style={styles.innerBox}>
+            <View style={styles.header}>
+              <Text style={styles.headerTitle}>Hello again</Text>
+              <Text style={styles.headerTitle}>Welcome</Text>
+            </View>
+
+            <View style={{ marginBottom: 20 }}>
+              <Text style={styles.inputTitle}>EMAIL</Text>
+              <TextInput
+                style={styles.input}
+                textAlign={"center"}
+                // onFocus={() => setIsShowKeyboard(true)}
+                value={state.email}
+                onChangeText={(value) =>
+                  setstate((prevState) => ({ ...prevState, email: value }))
+                }
+              />
+            </View>
+            <View style={{ marginBottom: 20 }}>
+              <Text style={styles.inputTitle}>PASSWORD</Text>
+              <TextInput
+                style={styles.input}
+                textAlign={"center"}
+                secureTextEntry={true}
+                // onFocus={() => setIsShowKeyboard(true)}
+                value={state.password}
+                onChangeText={(value) =>
+                  setstate((prevState) => ({ ...prevState, password: value }))
+                }
+              />
+            </View>
+            <TouchableOpacity
+              style={styles.btn}
+              activeOpacity={0.4}
+              onPress={keyboardHide}
+            >
+              <Text style={styles.btnTitle}>SING IN</Text>
+            </TouchableOpacity>
+          </View>
+          {/* </KeyboardAvoidingView> */}
+          {/* <View style={styles.innerBox}>
           <Text style={styles.text}>life is good...</Text>
         </View> */}
-      </ImageBackground>
-      <StatusBar style="auto" />
-    </View>
+        </ImageBackground>
+        <StatusBar style="auto" />
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -83,8 +168,9 @@ const styles = StyleSheet.create({
   },
 
   form: {
-    marginHorizontal: 40,
+    // marginHorizontal: 40,
     marginBottom: 40,
+    // backgroundColor: "#fff",
   },
 
   input: {
@@ -109,25 +195,38 @@ const styles = StyleSheet.create({
     marginHorizontal: 20,
     justifyContent: "center",
     alignItems: "center",
-    ...Platform.select({
-      ios: {
-        backgroundColor: "transparent",
-        borderColor: "#f0f8ff",
-      },
-      android: {
-        backgroundColor: "#ffb6c1",
-        borderColor: "transparent",
-      },
-    }),
+    // ...Platform.select({
+    //   ios: {
+    //     backgroundColor: "transparent",
+    //     borderColor: "#f0f8ff",
+    //   },
+    //   android: {
+    //     backgroundColor: "#ffb6c1",
+    //     borderColor: "transparent",
+    //   },
+    // }),
     // backgroundColor: Platform.OS === "ios" ? "transparent" : "#ffb6c1",
     // borderColor: Platform.OS === "ios" ? "#f0f8ff" : "transparent",
+    backgroundColor: "#ffb6c1",
+    borderColor: "transparent",
   },
 
   btnTitle: {
-    color: Platform.OS === "ios" ? "#4169e1" : `#2e8b57`,
+    // color: Platform.OS === "ios" ? "#4169e1" : `#2e8b57`,
+    color: `#2e8b57`,
     fontSize: 20,
   },
 
+  header: {
+    alignItems: "center",
+    marginBottom: 50,
+  },
+
+  headerTitle: {
+    fontSize: 30,
+    color: `#0000ff`,
+    fontFamily: "roboto-700",
+  },
   // text: {
   //   color: "blue",
   //   fontSize: 30,
