e6badd133a3eb53611d0bc5de7925a59dddae72d
Maxximusan <dragonstonecave@gmail.com>
2023-04-26 18:11:34 +0300

    5.2 Получение данных с камеры

--- Screens/mainScreens/CreatePostsScreen.jsx
+++ Screens/mainScreens/CreatePostsScreen.jsx
@@ -1,12 +1,28 @@
-import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
 import { Camera } from "expo-camera";
+// import { TouchableOpacity } from "react-native-gesture-handler";
 
 export const CreatePostsScreen = () => {
+  const [snap, setSnap] = useState(null);
+  const [foto, setFoto] = useState(null);
+
+  const takePhoto = async () => {
+    const photo = await snap.takePictureAsync();
+    setFoto(photo.uri);
+    // console.log("camera ---->", photo.uri);
+    console.log("photo", photo);
+  };
+
   return (
     <View style={styles.container}>
-      <Camera style={styles.camera}>
-        <TouchableOpacity style={styles.snapContainer} onPress={() => {}}>
+      <Camera style={styles.camera} ref={setSnap}>
+        {foto && (
+          <View style={styles.takePhotoContainer}>
+            <Image source={{ uri: foto }} style={{ height: 100, width: 100 }} />
+          </View>
+        )}
+        <TouchableOpacity style={styles.snapContainer} onPress={takePhoto}>
           <Text style={styles.snap}> SNAP </Text>
         </TouchableOpacity>
       </Camera>
@@ -17,13 +33,14 @@ export const CreatePostsScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // justifyContent: "center",
-    // alignItems: "center",
   },
 
   camera: {
     height: 300,
-    marginTop: 20,
+
+    marginTop: 32,
+    marginLeft: 16,
+    marginRight: 16,
 
     alignItems: "center",
   },
@@ -41,5 +58,13 @@ const styles = StyleSheet.create({
 
   snap: {
     color: "#fff",
+  },
+
+  takePhotoContainer: {
+    position: "absolute",
+    top: 10,
+    left: 10,
+    borderColor: "#fff",
+    borderWidth: 1,
   },
 });
