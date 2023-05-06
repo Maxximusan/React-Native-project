064f5bfe9a6a50f54633f911435b96a77dad3bdf
Maxximusan <dragonstonecave@gmail.com>
2023-05-06 17:02:16 +0300

    5.4 отрисовка коллекции данных
    useEfect, if (поскольку показывает undefined).
    Flatlist

--- Screens/mainScreens/PostsScreen.jsx
+++ Screens/mainScreens/PostsScreen.jsx
@@ -1,11 +1,36 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Image, StyleSheet, FlatList } from "react-native";
 
 export const PostsScreen = ({ route }) => {
+  const [posts, setPosts] = useState([]);
   console.log("route.params", route.params);
+
+  useEffect(() => {
+    if (route.params) {
+      setPosts((prevState) => [...prevState, route.params]);
+    }
+  }, [route.params]);
+  console.log("posts", posts);
   return (
     <View style={styles.container}>
-      <Text>PostsScreen</Text>
+      <FlatList
+        data={posts}
+        keyExtractor={(item, indx) => indx.toString()}
+        renderItem={({ item }) => (
+          <View
+            style={{
+              marginBottom: 10,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Image
+              source={{ uri: item.foto }}
+              style={{ width: 350, height: 200 }}
+            />
+          </View>
+        )}
+      />
     </View>
   );
 };
